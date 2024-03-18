71967cf945c69840842946e9ad75ffa79b997587
Sim-km <simkm0824@naver.com>
2024-03-18 17:57:00 +0900

    feat : chat gpt뿐만 아닌 다른 플랫폼의 api를 사용하기 위해 메소드 변경

--- llm_handler.py
+++ llm_handler.py
@@ -4,22 +4,24 @@ import logging
 
 import prompt
 
+def generate_review(review_target_code, request_api_key, llm_type='chat_gpt'):
+    if(llm_type == 'chat_gpt'):
+        return generate_message_from_chat_gpt(review_target_code, request_api_key)
+    return ""
 
-def generate_review(review_target_code, request_api_key):
+
+def generate_message_from_chat_gpt(review_target_code, request_api_key):
     client = OpenAI(api_key=request_api_key)
 
-    logging.getLogger().info("generating review")
+    logging.getLogger().info("generating message")
 
     response = client.chat.completions.create(
         model="gpt-3.5-turbo",
         messages=[
             {"role": "system",
-             "content": prompt.getCodeReviewSystemPrompt()},
+             "content": prompt.getChatGptSystemPrompt()},
             {"role": "user",
              "content": """
-            first summary provided code,
-            Review your provided code about method naming, logic flow, or handling exeception and provide a list of recommendations. and also provide a brief explanation of the code.
-
             {0}""".format(review_target_code)}
         ]
     )
